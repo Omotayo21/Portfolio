b675826a10347f70f0423657bdf9da9a137d387a
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2024-08-08 01:36:36 +0100

    small change to height

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -128,7 +128,7 @@ const Projects = forwardRef<HTMLDivElement> ((props, ref) => {
               </div>
             </div>
           </div>
-           <div className="h-[26rem] rounded-md border border-black w-[22rem]">
+           <div className="h-[31rem] rounded-md border border-black w-[22rem]">
             <img src={img11} className="w-96 h-40" />
             <div className="flex flex-col gap-y-4 ml-2">
               {" "}
